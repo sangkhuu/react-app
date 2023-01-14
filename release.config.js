5d3001fd2ddfdd2cffc81c0374c17af872be6582
sangkhuu <sang.khuu@karrostech.com>
2023-01-14 09:28:35 +0700

    fix: add release module

--- release.config.js
+++ release.config.js
@@ -2,9 +2,9 @@ module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/sangkhuu/react-app",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github'
-  ]
-}
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
