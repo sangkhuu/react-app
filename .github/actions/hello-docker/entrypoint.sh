148f6c77a91c8696ca90007c92e88fe9e91ff5f6
sangkhuu <sang.khuu@karrostech.com>
2023-01-16 23:20:39 +0700

    fix: testing docker

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+echo "::add-mask::$1"
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
+
+echo "::group:Some expandable logs"
+echo "some stuff"
+
+
+echo "::endgroup::"
+
+echo "some stuff"
+echo "::set-env name=Hello::hello"
