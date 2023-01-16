9b1575e6760f91b2c2b47d92e675368760055770
sangkhuu <sang.khuu@karrostech.com>
2023-01-16 11:06:22 +0700

    feat: add feature 2

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1169,7 +1169,7 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  // throw new Error("some error message");
+  throw new Error("some error message");
   const name = core.getInput('who-to-greet');
   console.log('Hello ${name}');
   
